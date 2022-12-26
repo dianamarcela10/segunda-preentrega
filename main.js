9750f74108b0856c89c6b23220b682c58167b167
diana guarin <dianamar8723@hotmail.com>
2022-12-26 18:58:41 -0500

    se creo css se modifico index

--- main.js
+++ main.js
@@ -1,38 +1,93 @@
+
+
+
+
+let boton = document.getElementById("btn")
+console.log(boton);
+
+
+
 const arrayProducts = [
-   
-   {name: 'blusa', price: 20000, size: 8, quantiy: 4 },
-   {name: 'pantalon', price: 20000, size: 8, quantiy: 4 },
-   {name: 'camisa', price: 20000, size: 8, quantiy: 4 },
-   {name: 'jeans', price: 20000, size: 8, quantiy: 4 },
-   
+
+  { name: 'blusa', price: 20000, size: 8, },
+  { name: 'zapatos', price: 80000, size: 8, },
+  { name: 'pulsera', price: 20000, size: 8, },
+  { name: 'jeans', price: 70000, size: 8, },
+
 
 ];
 
-function product(name, price, size, quantiy) {
-   this.name = name;
-   this.price = price;
-   this.size = size;
-   this.quantiy = quantiy;
+//function product(name, price, size, quantiy) {
+//  this.name = name;
+//  this.price = price;
+//  this.size = size;
+// this.quantiy = quantiy;
+
+//}
+
+// function addProduct() {
+//  const productName = prompt("que producto deseas?");
+//  const productPrice = parseInt(prompt("de que precio? "));
+//  const productSize = parseInt(prompt("que talla quieres"));
+//  const ProductQuantity = parseInt(prompt("que cantidad deseas"));
+
+
+//const newProduct = new product(productName, productPrice, productSize, ProductQuantity);
+
+// arrayProducts.push(newProduct);
+// return arrayProducts;
+// }
+
+function sumarProductos() {
+  return arrayProducts.reduce(function (acumu, pro) {
+    return acumu + pro.price;
+  }, 0)
 
 }
 
- function addProduct() {
-   const productName = prompt("que producto deseas?");
-   const productPrice = parseInt(prompt("de que precio? "));
-   const productSize = parseInt(prompt("que talla quieres"));
-   const ProductQuantity = parseInt(prompt("que cantidad deseas"));
+
+// por clase nos da un array//
 
 
-   const newProduct = new product(productName, productPrice, productSize, ProductQuantity);
+let flexcontainer = document.getElementsByClassName("flex-container");
+console.log(flexcontainer);
 
-   arrayProducts.push(newProduct);
-   return arrayProducts;
+// por objeto//
+
+
+
+class product {
+  constructor(categoria, price, size,) {
+    this.categoria = categoria;
+    this.price = price;
+    this.size = size;
+  }
 }
- 
- function sumarProductos() {
-  return arrayProducts.reduce(function(acumu,pro){ 
-  return acumu + pro.price;
-  },0)
-  
- }
- 
+
+
+const productosAgregdos = [];
+
+const crearProducto = (e) => { 
+  e.preventDefault();
+  let categoria = document.querySelector("#categoria").value;
+  let price = parseInt(document.querySelector("#price").value);
+  let size = document.querySelector("#size").value;
+
+  let newProducto = new product(categoria, price, size);
+  productosAgregdos.push(newProducto);
+  localStorage.setItem("productos", productosAgregdos);
+}
+
+ const escribir = () => {
+  let texto = alert(" producto agregado");
+
+}
+
+boton.addEventListener("click", crearProducto);
+
+
+
+
+
+
+
